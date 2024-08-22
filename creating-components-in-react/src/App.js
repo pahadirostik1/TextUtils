611724b3a3fb5f663fa8a4cd67f445ab91430181
Rostik <rostikpahadi@gmail.com>
2024-08-22 16:42:47 +0545

    For Text Utilities

---
+++ creating-components-in-react/src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+// import About from './Components/About';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import React, { useState } from 'react';
+
+function App() {
+    const [mode, setMode] = useState("light");
+
+    const toggleMode=()=>{
+      if(mode=== "light"){
+        setMode("dark");
+        document.body.style.backgroundColor= "#042743";
+      }
+      else {
+        setMode("light");
+        document.body.style.backgroundColor= "white";
+      }
+    }
+     
+  return (
+    <>
+  <Navbar title="TextUtils" mode={mode}  toggleMode={toggleMode} />;
+  <div className="container my-3">
+    <TextForm heading="Enter any text" mode={mode}/>
+   {/* <About/>; */}
+  </div>
+    </>
+   
+  );
+}
+
+export default App;
---
+++ creating-components-in-react/src/Components/TextForm.js
@@ -0,0 +1,65 @@
+import React ,{useState} from 'react'
+
+export default function TextForm(props) {
+    const HandleUp=()=>{
+        let newText=text.toUpperCase();
+        setText(newText);
+        
+    }
+    const HandleLow=()=>{
+        let newText=text.toLowerCase();
+        setText(newText);
+       
+    }
+    const HandleClear=()=>{
+        let newText=" ";
+        setText(newText);
+         
+    }  
+    const HandleCopy=()=>{
+        var text= document.querySelector("#myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const HandleExtraSpace=()=>{
+        var newText=text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+
+    const HandleChange=(event)=>{
+        console.log("On Change");
+        setText(event.target.value);
+    }
+
+    
+    const [text, setText]= useState("");
+  return (
+    <>
+    <div className='container' style={{color: props.mode==="dark" ? "white":"#042743"}}>
+        <label for="myBox"><h1>{props.heading}</h1></label>
+        <div className="mb-3">
+           <textarea className="form-control" value={text}  onChange={HandleChange} style={{backgroundColor: props.mode==="dark"?"grey":"white",
+            color: props.mode==="dark"?"black":"#042743"}} id="myBox"  rows="5"></textarea>
+        </div>
+        <button className="btn btn-primary mx-2 my-3" onClick={HandleUp}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={HandleLow}>Convert to LowerCase</button>
+        <button className="btn btn-primary mx-2" onClick={HandleClear}>Clear</button>
+        <button className="btn btn-primary mx-2" onClick={HandleCopy}>Copy Text</button>
+        <button className="btn btn-primary mx-2" onClick={HandleExtraSpace}>Remove Extra Spaces</button> 
+    </div>
+    <div className="container" style={{color: props.mode==="dark"?"white":"#042743"}} >
+        <h2> Your text contains </h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p> {0.008 *text.split(" ").length } minutes to read</p> 
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter any text to preview"}</p>
+    </div>
+    </>
+    )
+}
+            
+    
+    
+
+    
+      

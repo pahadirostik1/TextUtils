bc98459589f80e492c618d5b4ad8411bf844671b
Rostik <rostikpahadi@gmail.com>
2024-08-26 13:15:17 +0545

    Adding Color Palette

--- creating-components-in-react/src/App.js
+++ creating-components-in-react/src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
 import Alert from './Components/Alert';
-// import About from './Components/About';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import React, { useState } from 'react';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from  "react-router-dom";
 
 function App() {
     const [mode, setMode] = useState("light");
@@ -19,32 +25,51 @@ function App() {
         setAlert(null);
       },1000);
     }
+    const removeColor=()=>{
+      document.body.classList.remove("bg-success");
+      document.body.classList.remove("bg-warning");
+      document.body.classList.remove("bg-light");
+      document.body.classList.remove("bg-danger");
+      document.body.classList.remove("bg-dark");
+      // document.body.classList.remove("bg-primary");
+ }
 
-
-    const toggleMode=()=>{
+    const toggleMode=(cls)=>{
+      removeColor();
+      document.body.classList.add('bg-'+cls);
+      console.log(mode);
+     
       if(mode=== "light"){
         setMode("dark");
         document.body.style.backgroundColor= "#042743";
         showAlert("Dark Mode has been Enabled","success");
+        // document.title="TextUtils- Dark mode";
       }
       else {
         setMode("light");
         document.body.style.backgroundColor= "white";
         showAlert("Light Mode has been Enabled","success");
+        // document.title="TextUtils- Light mode";
       }
     }
      
   return (
     <>
-      <Navbar title="TextUtils" mode={mode}  toggleMode={toggleMode} />
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <TextForm  showAlert={showAlert} heading="Enter any text" mode={mode}/>
-      {/* <About/>; */}
-      </div>
-        </>
-      
-      );
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About mode={mode}/>} />
+            <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter any text" mode={mode} />} />
+          </Routes>
+        </div>
+   
+      </Router>
+     </> 
+    );
 }
-
 export default App;
+      
+    
+      
--- creating-components-in-react/src/Components/TextForm.js
+++ creating-components-in-react/src/Components/TextForm.js
@@ -23,6 +23,7 @@ export default function TextForm(props) {
         var text= document.querySelector("#myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Copied to Clipboard","success");
     }
     const HandleExtraSpace=()=>{
@@ -43,19 +44,19 @@ export default function TextForm(props) {
     <div className='container' style={{color: props.mode==="dark" ? "white":"#042743"}}>
         <label for="myBox"><h1>{props.heading}</h1></label>
         <div className="mb-3">
-           <textarea className="form-control" value={text}  onChange={HandleChange} style={{backgroundColor: props.mode==="dark"?"grey":"white",
-            color: props.mode==="dark"?"black":"#042743"}} id="myBox"  rows="5"></textarea>
+           <textarea className="form-control" value={text}  onChange={HandleChange} style={{backgroundColor: props.mode==="dark"?"#13466e":"white",
+            color: props.mode==="dark"?"white":"#042743"}} id="myBox"  rows="5"></textarea>
         </div>
-        <button className="btn btn-primary mx-2 my-3" onClick={HandleUp}>Convert to UpperCase</button>
-        <button className="btn btn-primary mx-2" onClick={HandleLow}>Convert to LowerCase</button>
-        <button className="btn btn-primary mx-2" onClick={HandleClear}>Clear</button>
-        <button className="btn btn-primary mx-2" onClick={HandleCopy}>Copy Text</button>
-        <button className="btn btn-primary mx-2" onClick={HandleExtraSpace}>Remove Extra Spaces</button> 
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={HandleUp}>Convert to UpperCase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={HandleLow}>Convert to LowerCase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={HandleClear}>Clear</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={HandleCopy}>Copy Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick={HandleExtraSpace}>Remove Extra Spaces</button> 
     </div>
     <div className="container" style={{color: props.mode==="dark"?"white":"#042743"}} >
         <h2> Your text contains </h2>
-        <p>{text.length=== 0 ? 0 : text.trim().split(" ").length} words and {text.replace(/\s+/g, '').length} characters</p>        
-        <p> {0.008 *text.split(" ").length } minutes to read</p> 
+        <p>{text.split(" ").filter((element)=>{return element.length!=0}).length} words and {text.replace(/\s+/g, '').length} characters</p>        
+        <p> {0.008 *text.split(" ").filter((element)=>{return element.length!=0}).length } minutes to read</p> 
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter any text to preview"}</p>
     </div>
